2dd91e9955f955b360da1d9ea0573859aae0c92f
Hennady95 <gena.trusevich@mail.ru>
2021-07-23 17:09:28 -0700

    add: header component

---
+++ src/components/navigation/index.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+export const Navigation = () => {
+    return <div className = "navigation">
+        <a><span className = "logo"></span></a>
+        <ul className = "navigation-menu">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>Classes</li>
+            <li>About us</li>
+            <li>Privacy</li>
+            <li>Services</li>
+        </ul>
+    </div>
+}
